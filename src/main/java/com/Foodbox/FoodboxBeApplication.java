1d5c89d84392f6d7544c51de2480beeeb4de24cc
Raheem-Sk <raheemshaik276@gmail.com>
2023-09-20 11:10:33 +0530

    Skeleton project

---
+++ src/main/java/com/Foodbox/FoodboxBeApplication.java
@@ -0,0 +1,13 @@
+package com.Foodbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodboxBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodboxBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Foodbox/FoodboxBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Foodbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodboxBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
